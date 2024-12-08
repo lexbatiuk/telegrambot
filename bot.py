25f1527f98c9444b198fcbdae5c1aab5f5164948
lexbatiuk <134221508+lexbatiuk@users.noreply.github.com>
2024-12-08 23:48:13 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import logging
 import asyncio
 from aiogram import Bot, Dispatcher
 from handlers import register_handlers
@@ -5,29 +6,43 @@ from scheduler import setup_scheduler
 from database import init_db
 import os
 
+# Настройка логирования
+logging.basicConfig(
+    level=logging.INFO,  # Уровень логирования: DEBUG, INFO, WARNING, ERROR, CRITICAL
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
+logger = logging.getLogger(__name__)
+
 # Получение данных из переменных окружения
 API_TOKEN = os.getenv('bot_token')
 
 if not API_TOKEN:
+    logger.critical("Переменная окружения `bot_token` не задана. Завершаем выполнение.")
     raise ValueError("Переменная окружения `bot_token` не задана.")
 
-# Инициализация бота и диспетчера
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
 async def main():
-    print("Бот запускается...")
+    logger.info("Запуск бота...")
     # Инициализация базы данных
     init_db()
+    logger.info("База данных инициализирована.")
 
     # Регистрация обработчиков
     register_handlers(dp)
+    logger.info("Обработчики зарегистрированы.")
 
     # Запуск планировщика
     setup_scheduler(bot)
+    logger.info("Планировщик запущен.")
 
     # Запуск бота
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except Exception as e:
+        logger.exception(f"Критическая ошибка: {e}")
