ed5053f4ee9903ee916fc2ae7868344c532ed488
lexbatiuk <134221508+lexbatiuk@users.noreply.github.com>
2024-12-09 00:14:17 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ from scheduler import setup_scheduler
 from database import init_db
 import os
 
-# Настройка логирования
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
@@ -14,7 +13,6 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-# Получение данных из переменных окружения
 API_TOKEN = os.getenv('bot_token')
 
 if not API_TOKEN:
@@ -26,19 +24,15 @@ dp = Dispatcher()
 
 async def main():
     logger.info("Запуск бота...")
-    # Инициализация базы данных
     init_db()
     logger.info("База данных инициализирована.")
 
-    # Регистрация обработчиков
     register_handlers(dp)
     logger.info("Обработчики зарегистрированы.")
 
-    # Запуск планировщика
     setup_scheduler(bot)
     logger.info("Планировщик запущен.")
 
-    # Запуск бота
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
