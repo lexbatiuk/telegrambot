bfc6ebe25d9c2efd26f605729b77a6f66ce2cf95
lexbatiuk <134221508+lexbatiuk@users.noreply.github.com>
2024-12-08 21:33:06 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 from telethon import TelegramClient
 import asyncio
-
-# Environment Variables for Render
 import os
-api_id = int(os.getenv('25028817'))  # Telegram API ID
-api_hash = os.getenv('5a53c8a24269d4d6bed55de5a431811c')  # Telegram API Hash
-bot_token = os.getenv('7708475257:AAHaMc2mE24P9mHvOKl59bHYaAMr9q2eGYQ')  # Bot Token from BotFather
+
+# Environment Variables for Railway or Render
+api_id = int(os.getenv('api_id'))  # Telegram API ID
+api_hash = os.getenv('api_hash')  # Telegram API Hash
+bot_token = os.getenv('bot_token')  # Bot Token from BotFather
 
 # Initialize Telegram Client
 client = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
@@ -13,7 +13,7 @@ client = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 async def main():
     # Test the bot by sending a message to yourself
     me = await client.get_me()
-    await client.send_message(me.id, "Hello from your bot running on Render!")
+    await client.send_message(me.id, "Hello from your bot running!")
     print("Message sent to yourself!")
 
 # Run the bot
