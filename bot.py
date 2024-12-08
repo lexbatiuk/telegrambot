ca48cb48c4402ddfa4bef03fdd429f61384399ec
lexbatiuk <134221508+lexbatiuk@users.noreply.github.com>
2024-12-08 23:54:39 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,48 +1,36 @@
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from handlers import register_handlers
+from database import init_db
+from aiogram import Bot, Dispatcher
 import logging
 import asyncio
-from aiogram import Bot, Dispatcher
-from handlers import register_handlers
-from scheduler import setup_scheduler
-from database import init_db
 import os
+from handlers import clean_inactive_users
 
-# Настройка логирования
-logging.basicConfig(
-    level=logging.INFO,  # Уровень логирования: DEBUG, INFO, WARNING, ERROR, CRITICAL
-    format="%(asctime)s - %(levelname)s - %(message)s",
-)
-
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Получение данных из переменных окружения
 API_TOKEN = os.getenv('bot_token')
 
 if not API_TOKEN:
-    logger.critical("Переменная окружения `bot_token` не задана. Завершаем выполнение.")
+    logger.critical("Переменная окружения `bot_token` не задана.")
     raise ValueError("Переменная окружения `bot_token` не задана.")
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
+def setup_cleanup_task(bot):
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(clean_inactive_users, "interval", hours=24, args=[bot])
+    scheduler.start()
+    logger.info("Планировщик очистки данных запущен.")
+
 async def main():
     logger.info("Запуск бота...")
-    # Инициализация базы данных
     init_db()
-    logger.info("База данных инициализирована.")
-
-    # Регистрация обработчиков
     register_handlers(dp)
-    logger.info("Обработчики зарегистрированы.")
-
-    # Запуск планировщика
-    setup_scheduler(bot)
-    logger.info("Планировщик запущен.")
-
-    # Запуск бота
+    setup_cleanup_task(bot)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except Exception as e:
-        logger.exception(f"Критическая ошибка: {e}")
+    asyncio.run(main())
