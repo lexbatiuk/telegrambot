40ebcbcfed842c1c028df984d17031274fb9578b
lexbatiuk <134221508+lexbatiuk@users.noreply.github.com>
2024-12-08 23:28:29 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,30 +1,33 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from handlers import register_handlers
+from scheduler import setup_scheduler
+from database import init_db
 import os
 
 # Получение данных из переменных окружения
 API_TOKEN = os.getenv('bot_token')
 
 if not API_TOKEN:
-    raise ValueError("Токен бота не задан. Проверь переменные окружения!")
+    raise ValueError("Переменная окружения `bot_token` не задана.")
 
 # Инициализация бота и диспетчера
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
-# Хранилище данных о каналах пользователей
-user_channels = {}
-
-# Регистрация обработчиков
-register_handlers(dp, user_channels)
-
-# Главная функция запуска
 async def main():
-    try:
-        await dp.start_polling(bot)
-    except Exception as e:
-        print(f"Ошибка: {e}")
+    print("Бот запускается...")
+    # Инициализация базы данных
+    init_db()
+
+    # Регистрация обработчиков
+    register_handlers(dp)
+
+    # Запуск планировщика
+    setup_scheduler(bot)
+
+    # Запуск бота
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
