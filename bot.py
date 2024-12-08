545153de7e4e43aa680cfaede47d84ff3d40ecac
lexbatiuk <134221508+lexbatiuk@users.noreply.github.com>
2024-12-08 23:22:17 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,45 +1,30 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-from telethon import TelegramClient
-from dotenv import load_dotenv
-import os
-
-# Импортируем функции из других файлов
 from handlers import register_handlers
-from scheduler import start_scheduler
-
-# Загружаем переменные окружения
-load_dotenv()
 
 # Получение данных из переменных окружения
+import os
 API_TOKEN = os.getenv('bot_token')
-API_ID = os.getenv('api_id')
-API_HASH = os.getenv('api_hash')
 
-if not API_TOKEN or not API_ID or not API_HASH:
+if not API_TOKEN:
     raise ValueError("Необходимые переменные окружения не заданы.")
 
 # Инициализация бота и диспетчера
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
-# Инициализация клиента Telethon
-client = TelegramClient('bot_session', API_ID, API_HASH)
+# Словарь для хранения каналов пользователей
+user_channels = {}
 
 # Регистрация обработчиков
-register_handlers(dp, client)
+register_handlers(dp, user_channels)
 
-# Главная асинхронная функция
+# Главная функция запуска
 async def main():
     try:
-        await client.start()  # Подключаем Telethon
-        await start_scheduler(client, bot)  # Запускаем планировщик задач
-        await dp.start_polling(bot)  # Запускаем aiogram
+        await dp.start_polling(bot)
     except Exception as e:
-        print(f"Произошла ошибка: {e}")
-    finally:
-        await client.disconnect()
+        print(f"Ошибка: {e}")
 
-# Запуск
 if __name__ == "__main__":
     asyncio.run(main())
