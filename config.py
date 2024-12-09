d90e702a226aa3f95392e8d8449b5828d62670e5
lexbatiuk <134221508+lexbatiuk@users.noreply.github.com>
2024-12-09 22:02:58 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,29 @@
 import os
 
+
 class Config:
-    BOT_TOKEN = os.getenv("bot_token")
-    API_ID = os.getenv("api_id")
-    API_HASH = os.getenv("api_hash")
-    WEBHOOK_URL = os.getenv("webhook_url")
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    API_ID = os.getenv("API_ID")
+    API_HASH = os.getenv("API_HASH")
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    WEBHOOK_URL = os.getenv("WEBHOOK_URL")
     TELEGRAM_PHONE = os.getenv("TELEGRAM_PHONE")
-    PORT = int(os.getenv("port", 3000))
+    TELEGRAM_PASSWORD = os.getenv("TELEGRAM_PASSWORD")
+    PORT = int(os.getenv("PORT", 3000))
 
     @classmethod
     def validate(cls):
-        missing_vars = [var for var in cls.__dict__ if var.isupper() and not getattr(cls, var)]
+        """Проверка всех обязательных переменных окружения."""
+        required_vars = [
+            cls.BOT_TOKEN,
+            cls.API_ID,
+            cls.API_HASH,
+            cls.DATABASE_URL,
+            cls.WEBHOOK_URL,
+            cls.TELEGRAM_PHONE,
+        ]
+        missing_vars = [
+            var_name for var_name, value in vars(cls).items() if var_name.isupper() and not value
+        ]
         if missing_vars:
-            raise ValueError(f"Missing environment variables: {', '.join(missing_vars)}")
-
-Config.validate()
+            raise ValueError(f"Missing required environment variables: {', '.join(missing_vars)}")
