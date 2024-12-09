db2e393845f8dbbdacd70794d3f45c01cb3bb5e5
lexbatiuk <134221508+lexbatiuk@users.noreply.github.com>
2024-12-09 22:05:21 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,29 +1,21 @@
 import os
 
-
 class Config:
-    BOT_TOKEN = os.getenv("BOT_TOKEN")
-    API_ID = os.getenv("API_ID")
-    API_HASH = os.getenv("API_HASH")
+    BOT_TOKEN = os.getenv("bot_token")
+    API_ID = os.getenv("api_id")
+    API_HASH = os.getenv("api_hash")
     DATABASE_URL = os.getenv("DATABASE_URL")
     WEBHOOK_URL = os.getenv("WEBHOOK_URL")
     TELEGRAM_PHONE = os.getenv("TELEGRAM_PHONE")
     TELEGRAM_PASSWORD = os.getenv("TELEGRAM_PASSWORD")
     PORT = int(os.getenv("PORT", 3000))
+    ALLOWED_USER_ID = os.getenv("ALLOWED_USER_ID")
 
     @classmethod
     def validate(cls):
-        """Проверка всех обязательных переменных окружения."""
-        required_vars = [
-            cls.BOT_TOKEN,
-            cls.API_ID,
-            cls.API_HASH,
-            cls.DATABASE_URL,
-            cls.WEBHOOK_URL,
-            cls.TELEGRAM_PHONE,
-        ]
         missing_vars = [
-            var_name for var_name, value in vars(cls).items() if var_name.isupper() and not value
+            var_name for var_name, var_value in cls.__dict__.items()
+            if not var_name.startswith("__") and var_value is None
         ]
         if missing_vars:
             raise ValueError(f"Missing required environment variables: {', '.join(missing_vars)}")
